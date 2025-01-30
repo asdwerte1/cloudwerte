1e85ae56d227c9a2ee911940223cb2682a829bcf
asdwerte1 <samuel@markhamhome.uk>
2025-01-30 11:05:00 +0000

    Updated UI content

--- webui/src/index.js
+++ webui/src/index.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom/client';
 
-function Icon({ title, image_route, url }) {
+function Icon({ title, description, image_route, url }) {
   return (
     <div className='container-icon'>
       <h2>{title}</h2>
+      <p>{description}</p>
       <img src={image_route} alt='Container logo' />
       <br />
       <a href={url} target='_blank' rel='noreferrer'><button>Launch</button></a>
@@ -24,9 +25,9 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div id='icons'>
       {Object.entries(containers).map(([name, info]) => (
-        <Icon key={name} title={name} image_route={info.imagePath} url={info.containerUrl} />
+        <Icon key={name} title={name} description={info.description} image_route={info.imagePath} url={info.containerUrl} />
       ))}
     </div>
   );
