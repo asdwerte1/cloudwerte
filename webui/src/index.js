222ba3a7511b960baf400be8e66613995401931f
asdwerte1 <samuel@markhamhome.uk>
2025-01-30 09:42:02 +0000

    React components defined, basic UI started

--- webui/src/index.js
+++ webui/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Icon({ title, image_route, url }) {
+  return (
+    <div className='container-icon'>
+      <h2>{title}</h2>
+      <img src={image_route} alt='Container logo' />
+      <br />
+      <a href={url} target='_blank' rel='noreferrer'><button>Launch</button></a>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [containers, setContainers] = useState({});
+
+  useEffect(() => {
+    fetch("./containers.json")
+      .then((response) => response.json())
+      .then((data) => setContainers(data))
+      .catch((error) => console.error("Error loading JSON" , error));
+  }, []);
+
+  return (
+    <div>
+      {Object.entries(containers).map(([name, info]) => (
+        <Icon key={name} title={name} image_route={info.imagePath} url={info.containerUrl} />
+      ))}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('react-section'));
+root.render(<App />);
