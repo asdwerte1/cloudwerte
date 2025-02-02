db1ad2d31c8f023e007f5a1834e1bba2aa212de7
asdwerte1 <samuel@markhamhome.uk>
2025-02-02 20:49:42 +0000

    Updated backend

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const SECRET_KEY = process.env.JWT_SECRET || "supersecret";
+const UNAME = process.env.LOGIN;
+const PWORD = process.env.PASSWORD;
 
 app.use(cors());
 app.use(express.json());
@@ -14,9 +16,9 @@ app.use(express.json());
 app.post("/api/login", (req, res) => {
     const { username, password } = req.body;
 
-    if (username === "admin" && password === "password") {
+    if (username === UNAME && password === PWORD) {
         const token = jwt.sign( { username }, SECRET_KEY, {expiresIn: "1h"});
-        return res.json({});
+        return res.json({ token });
     }
 
     res.status(401).json({ message: " Invalid username or password" });
@@ -27,7 +29,7 @@ const authenticateToken = (req, res, next) => {
     const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) { 
-        return res.status(403).json({ message: "Invalid token" });
+        return res.status(401).json({ message: "Invalid token" });
     }
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
